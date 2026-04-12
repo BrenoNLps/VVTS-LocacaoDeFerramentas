71e203db09803d204fbf49a456e95d7738e0028e
brenonlps <brenolopes.xy@gmail.com>
2026-04-12 01:09:07 -0300

    test: add UnitTest, TDD e Functional annotations

---
+++ src/test/java/br/ifsp/demo/annotation/TDD.java
@@ -0,0 +1,14 @@
+package br.ifsp.demo.annotation;
+
+import org.junit.jupiter.api.Tag;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// TDD.java
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Tag("TDD")
+public @interface TDD {}
