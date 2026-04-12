d511f0893a6ccc49d110944622303316c2397d51
brenonlps <brenolopes.xy@gmail.com>
2026-04-12 01:10:30 -0300

    test: add TDDTestSuite and UnitTestSuite

---
+++ src/test/java/br/ifsp/demo/suite/TDDTestSuite.java
@@ -0,0 +1,8 @@
+package br.ifsp.demo.suite;
+
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeTags("TDD")
+public class TDDTestSuite {}
