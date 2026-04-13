bb60880ff6f07384105d7dd3451b15e94106ab45
brenonlps <brenolopes.xy@gmail.com>
2026-04-13 00:57:05 -0300

    feat: create ProgressivePrices value object

---
+++ src/main/java/br/ifsp/demo/domain/ProgressivePrices.java
@@ -0,0 +1,15 @@
+package br.ifsp.demo.domain;
+
+import java.math.BigDecimal;
+
+public class ProgressivePrices {
+    private final BigDecimal dailyRate;
+    private final BigDecimal weeklyDailyRate;
+    private final BigDecimal monthlyDailyRate;
+
+    public ProgressivePrices(BigDecimal dailyRate, BigDecimal weeklyDailyRate, BigDecimal monthlyDailyRate) {
+        this.dailyRate = dailyRate;
+        this.weeklyDailyRate = weeklyDailyRate;
+        this.monthlyDailyRate = monthlyDailyRate;
+    }
+}
