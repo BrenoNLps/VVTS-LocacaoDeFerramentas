687145017791eebe5009c16e73c2c21b1a4fe3cf
brenonlps <brenolopes.xy@gmail.com>
2026-04-13 17:37:47 -0300

    feat: implement blank tool id validation for return from maintenance - closes #59

--- src/main/java/br/ifsp/demo/application/service/ReturnFromMaintenance.java
+++ src/main/java/br/ifsp/demo/application/service/ReturnFromMaintenance.java
@@ -1,11 +1,13 @@
 package br.ifsp.demo.application.service;
 
+import br.ifsp.demo.domain.exception.InvalidArgumentException;
 import br.ifsp.demo.domain.model.MaintenanceRecord;
 
 public class ReturnFromMaintenance {
 
     public MaintenanceRecord execute(String toolId) {
         if(toolId==null) throw new NullPointerException();
+        if(toolId.isBlank()) throw new InvalidArgumentException("ToolId");
         return null;
     }
 
