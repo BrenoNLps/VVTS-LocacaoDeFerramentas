e52696ec3f6a94188bf1edf1cd8ab3ab2f9cbf30
brenonlps <brenolopes.xy@gmail.com>
2026-04-13 00:10:45 -0300

    feat: create MaintenanceRecord domain entity

---
+++ src/main/java/br/ifsp/demo/domain/MaintenanceRecord.java
@@ -0,0 +1,18 @@
+package br.ifsp.demo.domain;
+
+import java.time.LocalDate;
+
+public class MaintenanceRecord {
+    private String id;
+    private LocalDate sentDate;
+    private LocalDate returnDate;
+    private boolean closed;
+
+    public void close(LocalDate returnDate) {
+
+    }
+
+    public boolean isOpen() {
+        return false;
+    }
+}
