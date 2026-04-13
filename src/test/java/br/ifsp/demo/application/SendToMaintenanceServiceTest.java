18204573a075b3f2b46d896ab25aac57bed3b69a
brenonlps <brenolopes.xy@gmail.com>
2026-04-13 00:15:05 -0300

    test: add failing test for null tool id

---
+++ src/test/java/br/ifsp/demo/application/SendToMaintenanceServiceTest.java
@@ -0,0 +1,27 @@
+package br.ifsp.demo.application;
+
+import br.ifsp.demo.annotation.TDD;
+import br.ifsp.demo.annotation.UnitTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.*;
+
+@UnitTest
+@TDD
+@ExtendWith(MockitoExtension.class)
+class SendToMaintenanceServiceTest {
+    @InjectMocks
+    SendToMaintenanceService sendToMaintenanceService;
+
+    @Test
+    @DisplayName("Should throw NullPointerException when tool id is null")
+    void shouldThrowNullPointerExceptionWhenToolIdIsNull() {
+        assertThatThrownBy(() -> sendToMaintenanceService.execute(null))
+                .isInstanceOf(NullPointerException.class);
+    }
+
+}
