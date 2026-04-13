1aae092ae3a897eb6561c69e4449f4434dfb7b20
brenonlps <brenolopes.xy@gmail.com>
2026-04-13 00:12:38 -0300

    feat: create SendToMaintenanceService application service

---
+++ src/main/java/br/ifsp/demo/application/SendToMaintenanceService.java
@@ -0,0 +1,11 @@
+package br.ifsp.demo.application;
+
+import br.ifsp.demo.domain.MaintenanceRecord;
+
+import java.util.UUID;
+
+public class SendToMaintenanceService {
+    public MaintenanceRecord execute(String id) {
+        return null;
+    }
+}
