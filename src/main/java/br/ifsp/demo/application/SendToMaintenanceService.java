716bfaf61b6891d2188b22cfe65962a1cf7ec813
brenonlps <brenolopes.xy@gmail.com>
2026-04-13 00:16:34 -0300

    feat: implement null check for tool id in SendToMaintenanceService - closes #51

--- src/main/java/br/ifsp/demo/application/SendToMaintenanceService.java
+++ src/main/java/br/ifsp/demo/application/SendToMaintenanceService.java
@@ -2,10 +2,12 @@ package br.ifsp.demo.application;
 
 import br.ifsp.demo.domain.MaintenanceRecord;
 
+import java.util.Objects;
 import java.util.UUID;
 
 public class SendToMaintenanceService {
-    public MaintenanceRecord execute(String id) {
+    public MaintenanceRecord execute(String toolId) {
+        Objects.requireNonNull(toolId);
         return null;
     }
 }
