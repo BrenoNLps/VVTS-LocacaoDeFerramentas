8ce6ef78fe5a74dd76543c5cac042e383b036611
brenonlps <brenolopes.xy@gmail.com>
2026-04-13 01:13:28 -0300

    feat: implement tool not found check in SendToMaintenanceService - closes #50

--- src/main/java/br/ifsp/demo/application/SendToMaintenanceService.java
+++ src/main/java/br/ifsp/demo/application/SendToMaintenanceService.java
@@ -1,14 +1,23 @@
 package br.ifsp.demo.application;
 
 import br.ifsp.demo.domain.MaintenanceRecord;
-
+import br.ifsp.demo.domain.Tool;
+import br.ifsp.demo.domain.repository.ToolRepository;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.Objects;
-import java.util.UUID;
 
 public class SendToMaintenanceService {
+    private final ToolRepository toolRepository;
+
+    public SendToMaintenanceService(ToolRepository toolRepository) {
+        this.toolRepository = toolRepository;
+    }
+
     public MaintenanceRecord execute(String toolId) {
         Objects.requireNonNull(toolId);
         if(toolId.isBlank()) throw new IllegalArgumentException();
+        Tool tool = toolRepository.findById(toolId);
+        if(tool == null) throw new EntityNotFoundException();
         return null;
     }
 }
