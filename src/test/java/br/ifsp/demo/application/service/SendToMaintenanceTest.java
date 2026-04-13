2c1dda330a1732a09b8dbe10987a1c3ae806e272
brenonlps <brenolopes.xy@gmail.com>
2026-04-13 13:26:04 -0300

    refactor: extract toolId and tool to BeforeEach setup

--- src/test/java/br/ifsp/demo/application/service/SendToMaintenanceTest.java
+++ src/test/java/br/ifsp/demo/application/service/SendToMaintenanceTest.java
@@ -8,6 +8,7 @@ import br.ifsp.demo.domain.ToolStatus;
 import br.ifsp.demo.domain.exception.EntityNotFoundException;
 import br.ifsp.demo.domain.exception.ToolInUseException;
 import br.ifsp.demo.domain.repository.ToolRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,9 +24,17 @@ import static org.mockito.Mockito.when;
 @TDD
 @ExtendWith(MockitoExtension.class)
 class SendToMaintenanceTest {
+    private Tool tool;
+    private String toolId;
     @Mock private ToolRepository toolRepository;
     @InjectMocks
     SendToMaintenance sendToMaintenance;
+
+    @BeforeEach
+    void setUp() {
+        toolId= UUID.randomUUID().toString();
+        tool = new Tool();
+    }
 
     @Test
     @DisplayName("Should throw NullPointerException when tool id is null")
@@ -52,8 +61,6 @@ class SendToMaintenanceTest {
     @Test
     @DisplayName("Should generate maintenance record and change tool status to maintenance when tool is available")
     void shouldGenerateMaintenanceRecordAndChangeToolStatusWhenToolIsAvailable() {
-        String toolId = UUID.randomUUID().toString();
-        Tool tool = new Tool();
         when(toolRepository.findById(toolId)).thenReturn(tool);
         MaintenanceRecord result= sendToMaintenance.execute(toolId);
         assertThat(result).isNotNull();
@@ -62,8 +69,6 @@ class SendToMaintenanceTest {
     @Test
     @DisplayName("Should throw ToolInUseException when tool status is rented")
     void shouldThrowToolInUseExceptionWhenToolStatusIsRented() {
-        String toolId = UUID.randomUUID().toString();
-        Tool tool = new Tool();
         tool.setStatus(ToolStatus.RENTED);
         when(toolRepository.findById(toolId)).thenReturn(tool);
         assertThatThrownBy(() -> sendToMaintenance.execute(toolId)).isInstanceOf(ToolInUseException.class);
