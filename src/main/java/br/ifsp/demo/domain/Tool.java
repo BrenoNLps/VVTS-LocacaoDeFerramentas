f6408b907cb11fefb668782aa0a7be46b22d2a32
brenonlps <brenolopes.xy@gmail.com>
2026-04-13 13:38:35 -0300

    feat: implement maintenance status check in Tool sendToMaintenance - closes #49

--- src/main/java/br/ifsp/demo/domain/Tool.java
+++ src/main/java/br/ifsp/demo/domain/Tool.java
@@ -1,5 +1,6 @@
 package br.ifsp.demo.domain;
 
+import br.ifsp.demo.domain.exception.ToolAlreadyInMaintenanceException;
 import br.ifsp.demo.domain.exception.ToolInUseException;
 import lombok.Setter;
 import java.time.LocalDate;
@@ -19,6 +20,7 @@ public class Tool {
 
     public MaintenanceRecord sendToMaintenance(LocalDate date) {
         if(status == ToolStatus.RENTED) throw new ToolInUseException(this.id);
+        if(status == ToolStatus.MAINTENANCE) throw new ToolAlreadyInMaintenanceException(this.id);
         status= ToolStatus.MAINTENANCE;
         MaintenanceRecord record = new MaintenanceRecord();
         this.maintenanceHistory.add(record);
