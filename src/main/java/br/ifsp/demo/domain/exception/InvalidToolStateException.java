95aa26abaeff2973b01eb82fcb7192c802fc4608
brenonlps <brenolopes.xy@gmail.com>
2026-04-14 04:05:25 -0300

    feat: create InvalidToolStateException

---
+++ src/main/java/br/ifsp/demo/domain/exception/InvalidToolStateException.java
@@ -0,0 +1,7 @@
+package br.ifsp.demo.domain.exception;
+
+public class InvalidToolStateException extends RuntimeException {
+    public InvalidToolStateException(String toolId, String state) {
+        super("Tool " + toolId + " is in invalid state for this operation: " + state);
+    }
+}
