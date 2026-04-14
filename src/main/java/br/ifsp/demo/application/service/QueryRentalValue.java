26744cd6facf2411d95c78055b1cb93865624a06
brenonlps <brenolopes.xy@gmail.com>
2026-04-14 05:52:38 -0300

    feat: create QueryRentalValue application service

---
+++ src/main/java/br/ifsp/demo/application/service/QueryRentalValue.java
@@ -0,0 +1,14 @@
+package br.ifsp.demo.application.service;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+
+public class QueryRentalValue {
+
+    public BigDecimal execute(List<String> toolIds, LocalDate startDate, LocalDate endDate){
+
+        return null;
+    }
+}
