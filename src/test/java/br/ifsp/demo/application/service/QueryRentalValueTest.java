36aeb4f9e66fc16ffbadaa4f8f0139d60912379e
brenonlps <brenolopes.xy@gmail.com>
2026-04-14 05:52:47 -0300

    test: add failing test for null tool id

---
+++ src/test/java/br/ifsp/demo/application/service/QueryRentalValueTest.java
@@ -0,0 +1,43 @@
+package br.ifsp.demo.application.service;
+
+import br.ifsp.demo.annotation.TDD;
+import br.ifsp.demo.annotation.UnitTest;
+import br.ifsp.demo.domain.model.ProgressivePrices;
+import br.ifsp.demo.domain.model.Tool;
+import br.ifsp.demo.domain.model.ToolStatus;
+import br.ifsp.demo.domain.repository.ToolRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class QueryRentalValueTest {
+    @Mock ToolRepository toolRepository;
+    @InjectMocks private QueryRentalValue queryRentalValue;
+
+    private Tool buildTool(ToolStatus status) {
+        ProgressivePrices prices = new ProgressivePrices(BigDecimal.TEN, BigDecimal.valueOf(8), BigDecimal.valueOf(6));
+        return new Tool(UUID.randomUUID().toString(), "Hammer", status, prices);
+    }
+
+    @Test @UnitTest @TDD //75
+    @DisplayName("Should throw NullPointerException when tool id is null")
+    void shouldThrowNullPointerExceptionWhenToolIdIsNull() {
+        List<String> toolIds = new ArrayList<>();
+        toolIds.add(null);
+        assertThatThrownBy(() -> queryRentalValue.execute(toolIds, LocalDate.now(), LocalDate.now().plusDays(5)))
+                .isInstanceOf(NullPointerException.class);
+    }
+}
