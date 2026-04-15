fc0113ac42f25867f81020686157084bd3b1ecc7
hikazudani <lhjundi@proton.me>
2026-04-15 00:59:26 -0300

    feat(us-12): Throw Exception when rental is not active - closes #19 closes #60

--- src/main/java/br/ifsp/demo/application/service/CancelRental.java
+++ src/main/java/br/ifsp/demo/application/service/CancelRental.java
@@ -6,6 +6,8 @@ import br.ifsp.demo.domain.model.RentalStatus;
 import br.ifsp.demo.domain.model.Tool;
 import br.ifsp.demo.domain.model.ToolStatus;
 import br.ifsp.demo.domain.repository.RentalRepository;
+import br.ifsp.demo.exception.RentalAlreadyCancelledException;
+import br.ifsp.demo.exception.RentalAlreadyFinalizedException;
 
 import static br.ifsp.demo.domain.model.RentalStatus.*;
 import static br.ifsp.demo.domain.model.ToolStatus.*;
@@ -21,6 +23,8 @@ public class CancelRental {
     public void execute(String rentalId) {
         Rental rental = rentalRepository.findById(rentalId);
         if (rental == null) throw new EntityNotFoundException("Rental", rentalId);
+        if (rental.getStatus() == CANCELLED) throw new RentalAlreadyCancelledException(rentalId);
+        if (rental.getStatus() == FINALIZED) throw new RentalAlreadyFinalizedException(rentalId);
         rental.cancel();
         rentalRepository.save(rental);
     }
