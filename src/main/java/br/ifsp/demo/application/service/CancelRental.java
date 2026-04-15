7736f11abc85a72ab7b1a2718254d17e11736cb2
hikazudani <lhjundi@proton.me>
2026-04-15 01:14:45 -0300

    feat(us-12): add blank or null validation for rental id - closes #61 #62

--- src/main/java/br/ifsp/demo/application/service/CancelRental.java
+++ src/main/java/br/ifsp/demo/application/service/CancelRental.java
@@ -1,6 +1,7 @@
 package br.ifsp.demo.application.service;
 
 import br.ifsp.demo.domain.exception.EntityNotFoundException;
+import br.ifsp.demo.domain.exception.InvalidArgumentException;
 import br.ifsp.demo.domain.model.Rental;
 import br.ifsp.demo.domain.model.RentalStatus;
 import br.ifsp.demo.domain.model.Tool;
@@ -8,6 +9,8 @@ import br.ifsp.demo.domain.model.ToolStatus;
 import br.ifsp.demo.domain.repository.RentalRepository;
 import br.ifsp.demo.exception.RentalAlreadyCancelledException;
 import br.ifsp.demo.exception.RentalAlreadyFinalizedException;
+
+import java.util.Objects;
 
 import static br.ifsp.demo.domain.model.RentalStatus.*;
 import static br.ifsp.demo.domain.model.ToolStatus.*;
@@ -21,6 +24,9 @@ public class CancelRental {
     }
 
     public void execute(String rentalId) {
+        if (rentalId == null || rentalId.isBlank()) {
+            throw new InvalidArgumentException("rentalId");
+        }
         Rental rental = rentalRepository.findById(rentalId);
         if (rental == null) throw new EntityNotFoundException("Rental", rentalId);
         rental.cancel();
