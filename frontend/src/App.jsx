e7048cdd5612d885d242d86f268f70d8c5675856
brenonlps <brenolopes.xy@gmail.com>
2026-05-12 22:50:23 -0300

    feat: add PublicRoute to redirect authenticated users

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PATHS } from "./routes/paths";
 import { Login, Register, Home, Rental, Maintenance } from "./pages";
 import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={PATHS.login} element={<Login />} />
-        <Route path={PATHS.register} element={<Register />} />
+        <Route path={PATHS.login} element={<PublicRoute><Login /></PublicRoute>} />
+        <Route path={PATHS.register} element={<PublicRoute><Register /></PublicRoute>} />
         <Route path={PATHS.home} element={<PrivateRoute><Home /></PrivateRoute>} />
         <Route path={PATHS.rental} element={<PrivateRoute><Rental /></PrivateRoute>} />
         <Route path={PATHS.maintenance} element={<PrivateRoute><Maintenance /></PrivateRoute>} />
