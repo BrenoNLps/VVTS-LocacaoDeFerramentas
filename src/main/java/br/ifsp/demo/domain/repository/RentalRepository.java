789e5804ad149177882064d5d58af6ce8027b840
brenonlps <brenolopes.xy@gmail.com>
2026-05-13 00:38:03 -0300

    feat: add findAll to RentalRepository

--- src/main/java/br/ifsp/demo/domain/repository/RentalRepository.java
+++ src/main/java/br/ifsp/demo/domain/repository/RentalRepository.java
@@ -2,9 +2,13 @@ package br.ifsp.demo.domain.repository;
 
 import br.ifsp.demo.domain.model.Rental;
 
+import java.util.List;
+
 public interface RentalRepository {
 
     void save(Rental rental);
 
     Rental findById(String id);
+
+    List<Rental> findAll();
 }
