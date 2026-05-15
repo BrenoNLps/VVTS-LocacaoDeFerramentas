7a85d054cf14a63a73b7d5e1513f33dff297428b
brenonlps <brenolopes.xy@gmail.com>
2026-05-15 02:25:48 -0300

    feat: add save and findAll to CustomerRepository

--- src/main/java/br/ifsp/demo/domain/repository/CustomerRepository.java
+++ src/main/java/br/ifsp/demo/domain/repository/CustomerRepository.java
@@ -2,6 +2,10 @@ package br.ifsp.demo.domain.repository;
 
 import br.ifsp.demo.domain.model.Customer;
 
+import java.util.List;
+
 public interface CustomerRepository {
+    void save(Customer customer);
     Customer findById(String id);
+    List<Customer> findAll();
 }
