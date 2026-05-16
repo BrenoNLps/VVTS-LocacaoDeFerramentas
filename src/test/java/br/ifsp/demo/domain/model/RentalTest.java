c0e7491fc7b797d6aaf03688e0942127b79354de
brenonlps <brenolopes.xy@gmail.com>
2026-05-16 00:11:06 -0300

    test: add mutation test for Rental getTools

---
+++ src/test/java/br/ifsp/demo/domain/model/RentalTest.java
@@ -0,0 +1,35 @@
+package br.ifsp.demo.domain.model;
+
+import br.ifsp.demo.annotation.Mutation;
+import br.ifsp.demo.annotation.UnitTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RentalTest {
+
+    private static final LocalDate START = LocalDate.of(2026, 1, 1);
+    private static final Customer CUSTOMER = new Customer("customer-1", "John Doe");
+    private static final ProgressivePrices PRICES = new ProgressivePrices(
+            BigDecimal.TEN, BigDecimal.valueOf(8), BigDecimal.valueOf(6)
+    );
+
+    private Tool buildTool(String id) {
+        return new Tool(id, "Hammer", ToolStatus.AVAILABLE, PRICES);
+    }
+
+    @Test
+    @UnitTest
+    @Mutation
+    @DisplayName("Should return tools provided at creation")
+    void shouldReturnToolsProvidedAtCreation() {
+        Tool tool = buildTool("tool-1");
+        Rental rental = Rental.create("rental-1", List.of(tool), START, CUSTOMER);
+        assertThat(rental.getTools()).containsExactly(tool);
+    }
+}
