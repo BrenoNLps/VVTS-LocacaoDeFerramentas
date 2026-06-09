831292cfc700c28481e4284375331cf12a5caefc
Bernardo_Filho <berna10.fernandes@gmail.com>
2026-06-09 17:30:57 -0300

    test: add ui test data factory

---
+++ src/test/java/br/ifsp/demo/ui/helpers/UiTestDataFactory.java
@@ -0,0 +1,20 @@
+package br.ifsp.demo.ui.helpers;
+
+import com.github.javafaker.Faker;
+
+public class UiTestDataFactory {
+
+    private static final Faker faker = new Faker();
+
+    public static String createName() {
+        return faker.name().fullName();
+    }
+
+    public static String createEmail() {
+        return faker.internet().emailAddress();
+    }
+
+    public static String createPassword() {
+        return faker.internet().password();
+    }
+}
